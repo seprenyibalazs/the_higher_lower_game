b481dd122b3d345f66eda472da4725bc395f12c0
SeprenyiBalazs <seprenyi.ba@gmail.com>
2022-11-15 20:48:45 +0100

    finished the game

--- gmae_data.py
+++ gmae_data.py
@@ -1,31 +1,31 @@
 celebrity = [
     {
         'name': 'Bruce Willis',
-        'hight': '183',
+        'height': '183',
         'description': 'actor',
         'from': 'USA'
     },
     {
         'name': 'Arnold Schwarzenegger',
-        'hight': '188',
+        'height': '188',
         'description': 'politician',
         'from': 'Austria'
     },
     {
         'name': 'Mark Zuckerberg',
-        'hight': '171',
+        'height': '171',
         'description': 'computer scientist',
         'from': 'USA'
     },
     {
         'name': 'Elon Musk',
-        'hight': '184',
+        'height': '184',
         'description': 'businessman',
         'from': 'South Africa'
     },
     {
         'name': 'Clint Eastwood',
-        'hight': '193',
+        'height': '193',
         'description': 'actor',
         'from': 'USA'
     }
--- higher_taller_game.py
+++ higher_taller_game.py
@@ -2,36 +2,42 @@ from gmae_data import celebrity
 import random
 
 
-def format_data(account):
-    account_name = account['name']
-    account_description = account['description']
-    account_from = account['from']
-    return f"{account_name}, a {account_description}, {account_from}"
-
-
-def check_answer(guess, a_height, b_height):
-    if a_height > b_height:
-        return guess == "a"
-    else:
-        return guess == "b"
-
-
-account_a = random.choice(celebrity)
+score = 0
+game_should_continue = True
 account_b = random.choice(celebrity)
-if account_a == account_b:
+
+while game_should_continue:
+
+    def format_data(account):
+        account_name = account['name']
+        account_description = account['description']
+        account_from = account['from']
+        return f"{account_name}, a {account_description}, {account_from}"
+
+    def check_answer(guess, a_height, b_height):
+        if a_height > b_height:
+            return guess == "a"
+        else:
+            return guess == "b"
+
+    account_a = account_b
     account_b = random.choice(celebrity)
+    if account_a == account_b:
+        account_b = random.choice(celebrity)
 
-print(f"Compare A: {format_data(account_a)}")
-print(f"Against B: {format_data(account_b)}")
+    print(f"Compare A: {format_data(account_a)}")
+    print(f"Against B: {format_data(account_b)}")
 
-guess = input("Who is highter? Type 'A' or 'B': ").lower()
+    guess = input("Who is highter? Type 'A' or 'B': ").lower()
 
-a_height_account = account_a["height_count"]
-b_height_account = account_b["height_count"]
+    a_height_account = account_a['height']
+    b_height_account = account_b['height']
 
-is_correct = check_answer(guess, a_height_account, b_height_account)
+    is_correct = check_answer(guess, a_height_account, b_height_account)
 
-if is_correct:
-    print("You're right!")
-else:
-    print("Sorry, that's wrong.")
+    if is_correct:
+        score += 1
+        print(f"You're right! Curremt score: {score}")
+    else:
+        game_should_continue = False
+        print(f"Sorry, that's wrong. Final score: {score}")
